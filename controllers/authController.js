4d9989b7bae9ac29cb38bc393437c2208dd7062c
jgodi96@gmail.com <jgodi96@gmail.com>
2022-02-11 13:53:11 -0800

    Added error handling

---
+++ controllers/authController.js
@@ -0,0 +1,26 @@
+import User from "../models/user.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError } from "../errors/index.js";
+
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("please provide all values");
+  }
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email already in use");
+  }
+
+  const user = await User.create({ name, email, password });
+  res.status(StatusCodes.OK).json({ user });
+};
+const login = (req, res) => {
+  res.send("login user");
+};
+const updateUser = (req, res) => {
+  res.send("update user");
+};
+
+export { register, login, updateUser };
--- server.js
+++ server.js
@@ -2,15 +2,27 @@ import express from "express";
 const app = express();
 import dotenv from 'dotenv'
 dotenv.config()
+import 'express-async-errors'
+
+//db and authenticateUser
 import connectDB from './db/connect.js'
+
+//routers
+import authRouter from './router/authRouter.js'
+import jobsRouter from './router/jobsRouter.js'
+
 //middleware
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
+app.use(express.json())
 
 app.get("/", (req, res) => {
-    throw new Error('error')
+
   res.send("Welcome!");
 });
+
+app.use('/api/v1/auth',authRouter)
+app.use('/api/v1/jobs',jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
