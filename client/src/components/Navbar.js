53150ab2649aabb391749146c76b6757256ac868
jgodi96@gmail.com <jgodi96@gmail.com>
2022-02-22 11:29:08 -0800

    added icons

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,36 @@
 import Wrapper from "../assets/wrappers/Navbar";
-import {FaHome} from 'react-icons/fa'
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+import Logo from "./Logo";
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>Navbar</h4>
-      <FaHome/>
+      <div className="nav-center">
+        <button
+          className="toggle-btn"
+          onClick={() => console.log("toggle sidebar")}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log("logout user")}
+          >
+            <FaUserCircle>john</FaUserCircle>
+          </button>
+          <div className="dropdown show-dropdown">
+              <button type="button" className="dropdown-btn" onClick={() => console.log("logout user")}>
+                  logout
+              </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
