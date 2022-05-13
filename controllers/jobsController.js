c5d2c03a2d1a6c7c50f9e840e57c406e15217987
jgodi96@gmail.com <jgodi96@gmail.com>
2022-05-13 14:35:20 -0700

    Delete and edit functionality for jobs completed

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -5,6 +5,7 @@ import {
   UnAuthenticatedError,
   NotFoundError,
 } from "../errors/index.js";
+import CheckPermissions from '../utils/CheckPermissions.js'
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -17,16 +18,19 @@ const createJob = async (req, res) => {
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
+
 const getAllJobs = async (req, res) => {
   const jobs = await Job.find({ createdBy: req.user.userId });
   res
     .status(StatusCodes.OK)
     .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
 };
+
 const updateJob = async (req, res) => {
-  const { id: jobId } = req.body;
+  const { id: jobId } = req.params;
 
   const { company, position } = req.body;
+
   if (!position || !company) {
     throw new BadRequestError("Please provide all values");
   }
@@ -35,15 +39,31 @@ const updateJob = async (req, res) => {
   if (!job) {
     throw new NotFoundError(`No Job with Id : ${jobId} `);
   }
-  const updateJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+  CheckPermissions(req.user,job.createdBy)
+
+  const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
     new: true,
     runValidators: true,
   });
-  res.status(StatusCodes.OK).json({updatedJob})
+
+  res.status(StatusCodes.OK).json({ updatedJob });
 };
+
 const deleteJob = async (req, res) => {
-  res.send("delete job");
+
+  const { id: jobId } = req.params;
+
+  const job = await Job.findOne({ _id: jobId });
+
+  if (!job) {
+    throw new NotFoundError(`No Job with Id : ${jobId} `);
+  }
+  CheckPermissions(req.user,job.createdBy)
+
+  await job.remove()
+  res.status(StatusCodes.OK).json({ msg:"Success! Job Removed" });
 };
+
 const showStats = async (req, res) => {
   res.send("show stats");
 };
