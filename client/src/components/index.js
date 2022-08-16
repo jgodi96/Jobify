752935ad3df00f0db3b9ab6dc04a435ece7ad80e
jgodi96@gmail.com <jgodi96@gmail.com>
2022-08-16 11:49:04 -0700

    stats page completed

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -7,8 +7,11 @@ import SmallSidebar from '../components/SmallSidebar'
 import FormRowSelect from './FormRowSelect.js'
 import SearchContainer from './SearchContainer'
 import JobsContainer from './JobsContainer'
+import StatsContainer from './StatsContainer'
+import ChartsContainer from './ChartsContainer'
+import Loading from './Loading'
 
 export {Logo,FormRow,Alert,Navbar,
     BigSidebar,
     SmallSidebar,FormRowSelect,SearchContainer,
-    JobsContainer}
+    JobsContainer,StatsContainer,ChartsContainer,Loading}
--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -27,6 +27,8 @@ import {
   EDIT_JOB_BEGIN,
   EDIT_JOB_SUCCESS,
   EDIT_JOB_ERROR,
+  SHOW_STATS_BEGIN,
+  SHOW_STATS_SUCCESS
 } from "./actions";
 
 const token = localStorage.getItem("token");
@@ -55,6 +57,9 @@ const initialState = {
   totalJobs: 0,
   numOfPages: 1,
   page: 1,
+  stats:{},
+  monthlyApplications:[],
+
 };
 
 const AppContext = React.createContext();
@@ -263,6 +268,19 @@ const AppProvider = ({ children }) => {
     }
 
   };
+const showStats = async () => {
+  dispatch({type:SHOW_STATS_BEGIN})
+  try{
+    const {data} = await authFetch('/jobs/stats')
+    dispatch({type:SHOW_STATS_SUCCESS,payload:{
+      stats:data.defaultStats,
+      monthlyApplications: data.monthlyApplications,
+    }})
+  }catch(error){
+    console.log(error.response)
+    // logoutUser()
+  }clearAlert()
+}
   return (
     <AppContext.Provider
       value={{
@@ -280,6 +298,8 @@ const AppProvider = ({ children }) => {
         setEditJob,
         deleteJob,
         editJob,
+        showStats,
+    
       }}
     >
       {children}
